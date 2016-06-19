acce4b7f65ea2fdbcc3320e49fd18abfb9d1fe56
KamillaKhabibrakhmanova <venessiel@gmail.com>
2016-06-19 15:31:25 -0400

    solution to parition problem

--- Chapter2-LinkedLists/2.4-PartitionLL.js
+++ Chapter2-LinkedLists/2.4-PartitionLL.js
@@ -3,6 +3,26 @@ Problem:  Write code to partition a linked list around a value x, such that all 
 come before all nodes greater than or equal to x.
 */
 
-function partitionLinkedList(x) {
-	
+/*
+@params{firstElement} - first element in the list
+@params{x} value to partition around
+*/
+
+function partitionLinkedList(firstElement, x) {
+	var prev = null;
+	var curr = firstElement;
+	var headNode = firstElement;
+
+	//if val is less than the partition, set it as the head to move it left
+	while ( prev.next ) {
+		if ( curr.val < x ) {
+			prev.next = curr.next;
+			curr.next = headNode;
+			headNode = curr;
+		}
+		prev = curr;
+		curr = curr.next;
+	}
 }
+
+//example: 3->10->11->2, paritioned around 5 becomes 2->3->10->11
