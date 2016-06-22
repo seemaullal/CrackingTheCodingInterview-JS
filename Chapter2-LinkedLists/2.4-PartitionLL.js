5018024b6c30b699f52b053dcfb8a965512213c8
KamillaKhabibrakhmanova <venessiel@gmail.com>
2016-06-21 21:12:10 -0400

    fixed solution

--- Chapter2-LinkedLists/2.4-PartitionLL.js
+++ Chapter2-LinkedLists/2.4-PartitionLL.js
@@ -11,18 +11,19 @@ come before all nodes greater than or equal to x.
 function partitionLinkedList(firstElement, x) {
 	var prev = firstElement;
 	var curr = firstElement.next;
-	var headNode = firstElement;
 
 	//if val is less than the partition, set it as the head to move it left
 	while ( prev.next ) {
-		if ( curr.val < x ) {
+		if ( curr.data < x ) {
 			prev.next = curr.next;
-			curr.next = headNode;
-			headNode = curr;
+			//make a copy so we can keep incrementing from the old spot
+			var newHead = new Node(curr.data, firstElement);
+			firstElement = newHead;
 		}
 		prev = curr;
 		curr = curr.next;
 	}
+	return firstElement;
 }
 
 //example: 3->10->11->2, paritioned around 5 becomes 2->3->10->11
